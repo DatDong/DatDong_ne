d89a233ba2a3696e31a8afbeca8cd65caf22ee95
DatDong <phatdat16102002@gmail.com>
2025-03-31 11:09:35 +0700

    DatDong |  fixed bug valid envents

--- codeWar/makeCoffee.js
+++ codeWar/makeCoffee.js
@@ -2,18 +2,23 @@ function howMuchCoffee(events) {
   if (!events || events.length === 0) {
     return 0;
   }
-  let coffeeCount = 0;
+  let countCoffee = 0;
   const validEvents = ["cw", "dog", "cat", "movie"];
-  for (let i = 0; i < events.length; i++) {
-    if (!validEvents.includes(events[i].toLowerCase())) {
-      continue;
+    for (let i = 0; i < events.length; i++) {
+        if(!validEvents.includes(events[i].toLowerCase())){
+            continue;
+        }
+        if(events[i] === events[i].toLowerCase()){
+            countCoffee ++;
+        }
+        else if(events[i] === events[i].toUpperCase()){
+            countCoffee +=2;
+        }
     }
-    if (events[i] === events[i].toLowerCase()) {
-      coffeeCount++;
-    } else if (events[i] === events[i].toUpperCase()) {
-      coffeeCount += 2;
-    }
-  }
-  return coffeeCount > 3 ? "You need extra sleep" : coffeeCount;
+    return countCoffee > 3 ? "You need extra sleep" : countCoffee;
 }
-console.log(howMuchCoffee(["CW", "cw", "other"]));
+
+
+// howMuchCoffee(["CW", "cw", "other"]);
+console.log(howMuchCoffee(["CW", "cw", "other"])); //3 
+// console.log(howMuchCoffee([]));
