1937c6d34599185f139886a9629c00935768ad90
DatDong <phatdat16102002@gmail.com>
2025-03-31 15:30:41 +0700

    DatDong |  Fixed fruit name

--- codeWar/cutFruits.js
+++ codeWar/cutFruits.js
@@ -1,12 +1,15 @@
 function cutFruits(fruits) {
+  const fruitsName = new Set(["apple", "banana", "pear", "grape", "peach", "plum", "cherry", "mango", "orange",
+    "watermelon", "cantaloupe", "apricot", "coconut", "pineapple", "blueberry", "pitaya", "hawthorn",
+    "pomegranate", "jujube", "persimmon", "mangosteen", "ginkgo", "lemon", "durian", "tomato","litchi","carambola", "fig"]);
   let result = [];
   for (let i = 0; i < fruits.length; i++) {
     let fruit = fruits[i];
-    if (fruit === "bomb") {
+    if(!fruitsName.has(fruit)){
       result.push(fruit);
       continue;
     }
-    if (fruit.length > 0) {
+    if (fruit.length >= 3) {
       let mid = Math.ceil(fruit.length / 2);
       result.push(fruit.slice(0, mid), fruit.slice(mid));
     } else {
@@ -15,4 +18,4 @@ function cutFruits(fruits) {
   }
   return result;
 }
-console.log(cutFruits(["apple","pear","banana","bomb"]));
+console.log(cutFruits(["kodeksx","carambola","grape","cantaloupe","ball","jujube","ball","pomegranate","blueberry","glassball"]));
