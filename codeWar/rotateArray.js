08e36e9e12272162b493cba94d33354d3c1da846
DatDong <phatdat16102002@gmail.com>
2025-04-01 10:22:13 +0700

    DatDong |  fixed rotate array

--- codeWar/rotateArray.js
+++ codeWar/rotateArray.js
@@ -1,11 +1,10 @@
 function rotate(array, n) {
     let len = array.length;
-    if (len === 0) return array;
-    
-    n = n % len;
-    if (n < 0) n += len;
-    
-    return array.slice(-n).concat(array.slice(0, -n));
+    n = n%len;
+    if(n<0){
+        n+=len;
+    }
+    return array.slice(-n).concat(array.slice(0,-n)); 
 }
 var data = [1, 2, 3, 4, 5];
 console.log(rotate(data, 2));
