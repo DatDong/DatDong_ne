f78cd7b00e3f7ccc9bcedd1519907d1771b53627
DatDong <phatdat16102002@gmail.com>
2025-04-04 13:05:59 +0700

    DatDong |  fixed partition constraints

--- codeWar/partition_constraints.js
+++ codeWar/partition_constraints.js
@@ -1,18 +1,18 @@
-function partConst(n, k, num) {
+function partConst(sum, k, num) {
   let count = 0;
-  for (let a = 1; a <= n; a++) {
+  for (let a = 1; a <= sum; a++) {
     if (a === num) {
       continue;
     }
-    for (let b = a; b <= n; b++) {
+    for (let b = a; b <= sum; b++) {
       if (b === num) {
         continue;
       }
-      for (let c = b; c <= n; c++) {
+      for (let c = b; c <= sum; c++) {
         if (c === num) {
           continue;
         }
-        if (a + b + c === n && k === 3) {
+        if (a + b + c === sum && k === 3) {
           count++;
         }
       }
